12fe24c922cbc6bf4683568ee4e1f38a38a53e81
lilia10010 <liliaxtz2@gmail.com>
2024-03-16 21:44:46 -0300

    feat: crete catalog page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,11 +21,13 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body>
-        <AuthProvider>
-          <Header />
-          <div className="flex-1">{children}</div>
-          <Footer />
-        </AuthProvider>
+        <div className="flex h-full flex-col">
+          <AuthProvider>
+            <Header />
+            <div className="flex-1">{children}</div>
+            <Footer />
+          </AuthProvider>
+        </div>
       </body>
     </html>
   );
